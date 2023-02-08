1d0d7c97b1291a16f9dcc0f6fac65cadabe2a634
zeeshan363 <zsarfraz363@gmail.com>
2023-02-08 11:16:50 +0500

    first commit

---
+++ src/app/MyComp/todo/todo.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { Todo } from "../../Todo"
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent {
+  // localItem : string;
+  localItem : any;
+  todos:Todo[];
+  // constructor() {
+  //   // this.localItem = localStorage.getItem('todos') || '{}'
+  //   this.localItem = localStorage.getItem("todos");
+  //   if(this.localItem === null){
+  //     this.todos = []
+  //   }
+  //   else{
+  //     this.todos = JSON.parse("todos")
+  //   }
+  // }
+
+  constructor() {
+    this.todos = [
+      {sno:2, title:"title 1", desc:"description", active:true},
+      {sno:3, title:"title 2", desc:"description", active:true},
+      {sno:3, title:"title 2", desc:"description", active:true}
+    ]
+  }
+
+  deleteTodo(todo: Todo){
+    console.log(todo)
+    const index = this.todos.indexOf(todo)
+    this.todos.splice(index,1)
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+  addTodo(todo:Todo){
+    console.log(todo)
+    this.todos.push(todo)
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  constructor(){
+    setTimeout(() => {
+      this.title = "Todo App"
+    }, 2000);
+  }
 }
